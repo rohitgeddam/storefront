238fce9acc6d4b892bff24e0335658fc5373374b
rohitgeddam <rohitgeddam2018@gmail.com>
2021-04-24 16:01:32 +0530

    create component itemCard

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="item-card">
+    <div class="item-card__container">
+      <div class="item-card__image">
+        <img :src="item.thumbnail" alt="" />
+      </div>
+
+      <div class="item-card__body">
+        <h4>{{ item.title }}</h4>
+        <h5>{{ item.category }}</h5>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  props: {
+    item: Object
+  },
+  data(){
+    return {
+      
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .item-card{
+    padding: 20px;
+    max-width: 200px;
+    height: auto;
+    box-shadow: 0px 5px 5px 5px rgba(0,0,0,0.1);
+    margin: 10px;
+  }
+  .item-card__container{
+    display: flex;
+    flex-direction: column;
+  }
+
+  .item-card__image img{
+    box-shadow: 0px 5px 5px 5px rgba(0,0,0,0.1);
+    width: 200px;
+    height: auto;
+    object-fit: cover;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <ItemCard v-for="item in items" :key="item.id" :item="item"/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ItemCard from "@/components/ItemCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    ItemCard,
   },
+
+  data(){
+    return {
+    items: [
+      {
+        id: 1,
+        category: 'saree',
+        title: 'Designer Saree',
+        description: 'Indian Saree',
+        color: 'black',
+        price: 20.99,
+        thumbnail: "https://i.imgur.com/8PAWlEd.jpeg",
+        images: [
+          "https://i.imgur.com/8PAWlEd.jpeg",
+          "https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/4/2497sr03-463.jpg"
+        ]
+      },
+
+      {
+        id: 1,
+        category: 'saree',
+        title: 'Designer Saree',
+        description: 'Indian Saree',
+        color: 'black',
+        price: 20.99,
+        thumbnail: "https://i.imgur.com/8PAWlEd.jpeg",
+        images: [
+          "https://i.imgur.com/8PAWlEd.jpeg",
+          "https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/4/2497sr03-463.jpg"
+        ]
+      },
+
+      {
+        id: 1,
+        category: 'saree',
+        title: 'Designer Saree',
+        description: 'Indian Saree',
+        color: 'black',
+        price: 20.99,
+        thumbnail: "https://i.imgur.com/8PAWlEd.jpeg",
+        images: [
+          "https://i.imgur.com/8PAWlEd.jpeg",
+          "https://assets.panashindia.com/media/catalog/product/cache/1/small_image/262x377/9df78eab33525d08d6e5fb8d27136e95/2/4/2497sr03-463.jpg"
+        ]
+      },
+    ]
+    }
+    
+  }
 };
 </script>
+
+<style scoped>
+  .container{
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
